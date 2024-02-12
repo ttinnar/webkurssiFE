06f3b98f2bbd0de540389a8d6fb6298ac9874366
ttinnar <111982172+ttinnar@users.noreply.github.com>
2024-02-12 19:06:10 +0200

    upd

--- viikko4/vite.demo/vite-project/main.js
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = "ok";
-
-setupCounter(document.querySelector('button'));
